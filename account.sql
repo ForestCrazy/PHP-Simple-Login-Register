2f4e36d72988e8040ce4ede92ecd7d0ed80b0c03
ForestCrazy <controt123@gmail.com>
2020-06-25 03:16:33 +0700

    Add file via upload

---
+++ account.sql
@@ -0,0 +1,13 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+07:00";
+
+CREATE TABLE IF NOT EXISTS `account` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `username` varchar(300) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `password` VARCHAR(600) NOT NULL,
+  `salt` varchar(255),
+  `createtime` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
