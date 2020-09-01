dcbdc1c57a57e8a5bcc79c0741558160dfce1819
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2020-09-01 21:42:08 +0700

    add comment

--- logout.php
+++ logout.php
@@ -1,7 +1,10 @@
 <?php
 session_start();
+// เริ่มการทำงาน session
+
 session_destroy();
+// ทำลาย หรือ ยกเลิก session ทั้งหมดที่เรากำหนดไว้
+
+header("Location: index.php");
+// ให้ redirect หรือ พา ไปหน้า index.php
 ?>
-<script langquage='javascript'>
-    window.location = "login.php";
-</script>
